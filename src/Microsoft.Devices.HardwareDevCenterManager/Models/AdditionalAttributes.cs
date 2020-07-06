93f3caf6fbefc6ca32355d36eae993b96b2fd91c
Ben <v-benca@microsoft.com>
2020-07-06 14:29:07 -0700

    add AdditionalAttributes

---
+++ src/Microsoft.Devices.HardwareDevCenterManager/Models/AdditionalAttributes.cs
@@ -0,0 +1,22 @@
+﻿/*++
+    Copyright (c) Microsoft Corporation. All rights reserved.
+
+    Licensed under the MIT license.  See LICENSE file in the project root for full license information.  
+--*/
+using Newtonsoft.Json;
+
+namespace Microsoft.Devices.HardwareDevCenterManager.DevCenterApi
+{
+    public class AdditionalAttributes
+    {
+        [JsonProperty("storageController")]
+        public StorageController StorageController { get; set; }
+
+        [JsonProperty("raidController")]
+        public RaidController RaidController { get; set; }
+
+        [JsonProperty("svvp")]
+        public Svvp Svvp { get; set; }
+
+    }
+}
