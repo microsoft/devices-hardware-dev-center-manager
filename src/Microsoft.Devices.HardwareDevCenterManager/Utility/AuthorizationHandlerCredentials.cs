9908305b03594802a170f4527f29e54c01e2edfd
Ben Carpenter 🛫 <carpenterben@microsoft.com>
2024-08-02 11:56:25 -0700

    add credential options

--- src/Microsoft.Devices.HardwareDevCenterManager/Utility/AuthorizationHandlerCredentials.cs
+++ src/Microsoft.Devices.HardwareDevCenterManager/Utility/AuthorizationHandlerCredentials.cs
@@ -4,30 +4,46 @@
     Licensed under the MIT license. See LICENSE file in the project root for full license information.
 --*/
 
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.Devices.HardwareDevCenterManager.Utility;
 
 public class AuthorizationHandlerCredentials
 {
-    [JsonPropertyName("key")]
-    public string Key { get; set; }
-
-    [JsonPropertyName("clientId")]
-    public string ClientId { get; set; }
+    // used for Azure
+    [JsonPropertyName("authority")]
+    public string Authority { get; set; }
 
     [JsonPropertyName("tenantId")]
     public string TenantId { get; set; }
 
-    [JsonPropertyName("url")]
-    public System.Uri Url { get; set; }
-
-    [JsonPropertyName("urlPrefix")]
-    public System.Uri UrlPrefix { get; set; }
+    [JsonPropertyName("clientId")]
+    public string ClientId { get; set; }
 
     [JsonPropertyName("managedIdentityClientId")]
     public string ManagedIdentityClientId { get; set; }
 
     [JsonPropertyName("scope")]
     public string Scope { get; set; }
+
+    [JsonPropertyName("x509Certificate2")]
+    public X509Certificate2 X509Certificate2 { get; set; }
+
+    [JsonPropertyName("x509Certificate2Name")]
+    public string X509Certificate2Name { get; set; }
+
+    [JsonPropertyName("keyVaultUrl")]
+    public string KeyVaultUrl { get; set; }
+
+
+    // used for HDC
+    [JsonPropertyName("key")]
+    public string Key { get; set; }
+
+    [JsonPropertyName("url")]
+    public System.Uri Url { get; set; }
+
+    [JsonPropertyName("urlPrefix")]
+    public System.Uri UrlPrefix { get; set; }
 }
