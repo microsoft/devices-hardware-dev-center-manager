abc9668f64cfb6ed3fcaf4e4104950ddcb7024a6
Ben Carpenter 🛫 <carpenterben@microsoft.com>
2025-01-29 14:34:08 -0800

    update CoEngDriverPublishInfo add  LongToStringJsonConverter to properties to handle Dashboard documentation

--- src/Microsoft.Devices.HardwareDevCenterManager/Models/CoEngDriverPublishInfo.cs
+++ src/Microsoft.Devices.HardwareDevCenterManager/Models/CoEngDriverPublishInfo.cs
@@ -1,18 +1,21 @@
 ﻿/*++
     Copyright (c) Microsoft Corporation. All rights reserved.
 
-    Licensed under the MIT license. See LICENSE file in the project root for full license information.  
+    Licensed under the MIT license. See LICENSE file in the project root for full license information.
 --*/
 
+using Microsoft.Devices.HardwareDevCenterManager.Utility;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.Devices.HardwareDevCenterManager.DevCenterApi;
 
 public class CoEngDriverPublishInfo
 {
+    [JsonConverter(typeof(LongToStringJsonConverter))]
     [JsonPropertyName("flooringBuildNumber")]
     public string FlooringBuildNumber { get; set; }
 
+    [JsonConverter(typeof(LongToStringJsonConverter))]
     [JsonPropertyName("ceilingBuildNumber")]
     public string CeilingBuildNumber { get; set; }
 }
