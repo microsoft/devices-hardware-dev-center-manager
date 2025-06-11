cf0b5385a8bec292152d04ed725d4781fc5b0423
Ben Carpenter <carpenterben@microsoft.com>
2025-06-11 16:01:52 -0700

    update Svvp MaxProcessors and MaxMemory with LongToStringJsonConverter (#37)

--- src/Microsoft.Devices.HardwareDevCenterManager/Models/Svvp.cs
+++ src/Microsoft.Devices.HardwareDevCenterManager/Models/Svvp.cs
@@ -4,15 +4,18 @@
     Licensed under the MIT license. See LICENSE file in the project root for full license information.
 --*/
 
+using Microsoft.Devices.HardwareDevCenterManager.Utility;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.Devices.HardwareDevCenterManager.DevCenterApi;
 
 public class Svvp
 {
+    [JsonConverter(typeof(LongToStringJsonConverter))]
     [JsonPropertyName("maxProcessors")]
     public string MaxProcessors { get; set; }
 
+    [JsonConverter(typeof(LongToStringJsonConverter))]
     [JsonPropertyName("maxMemory")]
     public string MaxMemory { get; set; }
 }
